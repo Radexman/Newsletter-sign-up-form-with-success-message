ecd817d46540a31be9986449471993c2676dc272
Radosław Siek <Borderlandsmaniak@gmail.com>
2023-08-03 11:05:04 +0200

    Basic Setup

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,26 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['dist/*html'],
-  darkMode: 'class',
-  theme: {
-    screens: {
-      sm: '480px',
-      md: '768px',
-      lg: '1020px',
-      xl: '1440px',
-    },
-    extend: {},
-  },
-  plugins: [],
+	content: ['dist/*html'],
+	darkMode: 'class',
+	theme: {
+		screens: {
+			sm: '480px',
+			md: '768px',
+			lg: '1020px',
+			xl: '1440px',
+		},
+		extend: {
+			colors: {
+				tomato: 'hsl(4, 100%, 67%)',
+				darkSlateGray: 'hsl(234, 29%, 20%)',
+				charcoalGray: 'hsl(235, 18%, 26%)',
+				gray: 'hsl(231, 7%, 60%)',
+				white: 'hsl(0, 0%, 100%)',
+			},
+			fontFamily: {
+				sans: ['Roboto', 'sans-serif'],
+			},
+		},
+	},
+	plugins: [],
 };
